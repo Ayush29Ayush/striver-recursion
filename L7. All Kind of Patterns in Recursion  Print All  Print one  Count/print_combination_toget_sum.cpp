2e7f195f5bed2eb55f6f6466294a9cddd8159828
Ayush Senapati <ayushsenapati123@gmail.com>
2022-08-13 18:59:04 +0530

    Added L7

---
+++ L7. All Kind of Patterns in Recursion  Print All  Print one  Count/print_combination_toget_sum.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void printS(int index, vector<int> &ds, int s, int sum, int arr[], int n)
+{
+    //! base case
+    if(index==n)
+    {
+        if(s==sum)
+        {
+            for(auto i : ds)
+                cout<<i<<" ";
+            cout<<endl;
+        }
+        return;
+    }
+    //! TAKE
+    ds.push_back(arr[index]);
+    s += arr[index];
+    printS(index+1, ds, s, sum, arr, n);
+
+    //! NOT TAKE
+    ds.pop_back();
+    s -= arr[index];
+    printS(index+1, ds, s, sum, arr, n);
+}
+
+int main()
+{
+    int arr[] = {1,2,1};
+    int n = 3;
+    int sum = 2;
+    vector<int> ds;
+    printS(0, ds, 0, sum, arr, n);
+    return 0;
+}
